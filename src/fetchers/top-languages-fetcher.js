f00bf37a0728df95475e3e8118d35d08fa066380
Muhammad Azzuhair <15320135+msazzuhair@users.noreply.github.com>
2022-09-06 17:32:06 +0700

    Update top-languages-fetcher.js

--- src/fetchers/top-languages-fetcher.js
+++ src/fetchers/top-languages-fetcher.js
@@ -13,8 +13,8 @@ const fetcher = (variables, token) => {
       query: `
       query userInfo($login: String!) {
         user(login: $login) {
-          # fetch only owner repos & not forks
-          repositories(ownerAffiliations: OWNER, isFork: false, first: 100) {
+          # fetch non forks
+          repositories(isFork: false, first: 100) {
             nodes {
               name
               languages(first: 10, orderBy: {field: SIZE, direction: DESC}) {
